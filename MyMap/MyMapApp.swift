ad48ab2b21480527a3e70d227844f7e24bb754c4
nogu66 <ezochi66@gmail.com>
2023-05-27 11:49:17 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by YutaNoguchi on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
